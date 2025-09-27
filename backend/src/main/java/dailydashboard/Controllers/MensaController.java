e4fde8f85675c4d4dd4cb158c4eec12011a54c7f
joserigel <joserigel18@gmail.com>
2025-09-27 12:58:41 +0200

    finished mensa controller

---
+++ backend/src/main/java/dailydashboard/Controllers/MensaController.java
@@ -0,0 +1,80 @@
+package dailydashboard.Controllers;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dailydashboard.Enums.MensaLocation;
+
+@RestController
+public class MensaController {
+
+    private MensaLocation matchMensaLocation(Optional<String> location) {
+        if (!location.isPresent()) {
+            return MensaLocation.ACADEMICA;
+        }
+
+        String locationString = location.get();
+        for (MensaLocation mensa: MensaLocation.values()) {
+            if (locationString.equals(mensa.getLabel())) {
+                return mensa;
+            }
+        }
+        throw new Error(String.format("Mensa location %s not found!", locationString));
+    }
+
+    @SuppressWarnings("unchecked")
+    @GetMapping(path = "/mensa-menu")
+    public @ResponseBody JSONAware menu(@RequestParam("location") Optional<String> location) {
+        try {
+            MensaLocation mensaLocation = matchMensaLocation(location);
+            
+            Document doc = Jsoup.connect(mensaLocation.getUrl()).get();
+            Elements elements = doc.body().selectFirst("* div.accordion").children();
+            
+            JSONArray days = new JSONArray();
+            for (Element element: elements.asList()) {
+                JSONObject menuJson = new JSONObject();
+                
+                Element header = element.selectFirst(".default-headline").children().first();
+                menuJson.put("date", header.text());
+
+                Elements menus = element.selectFirst(".menues").selectFirst("tbody").children();
+                JSONArray menusJson = new JSONArray();
+                for (Element menu: menus.asList()) {
+                    Element desc = menu.selectFirst(".expand-nutr");
+                    desc.children().forEach((tag) -> tag.remove());
+                    Element price = menu.select(".menue-item").last();
+
+                    JSONObject itemJson = new JSONObject();
+                    itemJson.put("name", desc.text());
+                    itemJson.put("price", price.text());
+
+                    menusJson.add(itemJson);
+                }
+
+                menuJson.put("menu", menusJson);
+
+                days.add(menuJson);
+            }
+            
+
+            return days;
+        } catch (Exception e) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("error", e.getMessage());
+            return new JSONObject(hm);
+        }
+    }
+}
---
+++ backend/src/main/java/dailydashboard/Enums/MensaLocation.java
@@ -0,0 +1,21 @@
+package dailydashboard.Enums;
+
+public enum MensaLocation {
+    ACADEMICA("academica", "https://www.studierendenwerk-aachen.de/speiseplaene/academica-w-en.html"),
+    AHORNSTRASSE("ahornstrasse", "https://www.studierendenwerk-aachen.de/speiseplaene/ahornstrasse-w-en.html");
+
+    private final String label;
+    private final String url;
+    private MensaLocation(String label, String url) {
+        this.url = url;
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+}
